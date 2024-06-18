e31df4a13e19b1a179fcc014a3f3a1fb85e5c187
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2024-06-18 18:23:55 +0200

    gson + adventure relocation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     java
     id("io.github.goooler.shadow") version "8.1.7"
@@ -9,10 +11,17 @@ repositories {
     mavenLocal()
 }
 
+val adventure = configurations.register("adventure")
+
+configurations.compileClasspath.configure {
+    extendsFrom(adventure.get())
+}
+
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
-    implementation("net.kyori:adventure-api:4.17.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.3.3")
+    implementation("com.google.code.gson:gson:2.11.0")
+    adventure("net.kyori:adventure-api:4.17.0")
+    adventure("net.kyori:adventure-platform-bukkit:4.3.3")
 }
 
 java {
@@ -21,13 +30,20 @@ java {
 
 tasks {
     shadowJar {
-        archiveClassifier = null
-        relocate("net.kyori", "de.dasbabypixel.adventuretesting.libs.kyori")
+        destinationDirectory = temporaryDir
+        exclude("com.google.errorprone")
+        relocate("com.google.gson", "de.dasbabypixel.adventuretesting.libs.gson")
     }
     jar {
         destinationDirectory = temporaryDir
     }
+    val finalJar = register<ShadowJar>("finalJar") {
+        from(shadowJar)
+        dependsOn(adventure)
+        configurations = listOf(adventure.get())
+        relocate("net.kyori", "de.dasbabypixel.adventuretesting.libs.kyori")
+    }
     assemble {
-        dependsOn(shadowJar)
+        dependsOn(finalJar)
     }
 }
